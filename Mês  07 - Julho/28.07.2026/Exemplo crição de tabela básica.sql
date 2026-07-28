ad8c2ac114c549a393ecb965ac5fbb28853a5084
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-07-28 20:58:32 -0300

    Adiciona exemplo de tabela básica no mysql

---
+++ Mês  07 - Julho/28.07.2026/Exemplo crição de tabela básica.sql
@@ -0,0 +1,16 @@
+	
+	
+	
+	CREATE DATABASE meu_banco;
+	
+	USE meu_banco;
+	
+	CREATE TABLE aluno (
+	id INT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	CPF CHAR(11),
+	data_nascimento DATE
+	);
+	
+	
